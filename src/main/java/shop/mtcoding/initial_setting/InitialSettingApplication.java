0b6f388b9ac94b243b4654a387e62077ed2c1c7d
aozp73 <aozp73@naver.com>
2023-04-04 09:10:07 +0900

    초기 커밋

---
+++ src/main/java/shop/mtcoding/initial_setting/InitialSettingApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.initial_setting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialSettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitialSettingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/initial_setting/InitialSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.initial_setting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialSettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
